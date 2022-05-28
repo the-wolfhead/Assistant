12cdc9a718f2915ca4254bb47c7a3310eda962b0
Daniel Otabor <danieldamianotabor@gmail.com>
2022-05-28 02:09:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web'
 
+const alanKey = '8924dbaea0fe28da4865d82f4be399c12e956eca572e1d8b807a3e2338fdd0dc/stage';
 function App() {
+
+  useEffect(() => {
+    alanBtn({
+      key:alanKey,
+      onCommand: ({command}) => {
+        if (command === 'testCommand') {
+          alert('this code was excuted');
+        }
+      }
+    })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>ALAN AI</h1>
     </div>
   );
 }
